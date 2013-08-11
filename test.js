89bf15773bcfd0c4b1c73eed43f64f90f81a23e8
kikoshoung <kiko.xiang@guohead.com>
2013-08-11 15:50:47 +0800

    modei

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var gpio = require("pi-gpio");
-
+console.log(gpio);
 gpio.open(11, "output", function(err) {       
     gpio.write(11, 1, function() {            
         gpio.close(11);            
