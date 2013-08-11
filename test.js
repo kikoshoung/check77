e2a04852856b25ca48687a2b86b5b0ab26bf677f
kikoshoung <kiko.xiang@guohead.com>
2013-08-11 16:51:27 +0800

    modi

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-var gpio = require("pi-gpio");
-console.log(gpio);
-gpio.open(11, "output", function(err) {       
-    gpio.write(11, 1, function() {            
-        gpio.close(11);            
-    });
-});
+var gpio = require("pi-gpio"),
+	pins = [11, 12, 13, 14],
+	pinIndex = 0,
+	timer;
+
+// setInterval(setPin, 500);
+setPin();
+
+function setPin(){
+	var currentPin = pins[pinIndex];
+	gpio.open(currentPin, "output", function(err) {       
+	    gpio.write(currentPin, 1, function() {            
+	        gpio.close(currentPin);
+	        timer = setTimeout(function(){
+	        	pinIndex++;
+	        	if(pinIndex > 3) pinIndex = 0;
+	        	setPin();
+	        }, 500);        
+	    });
+	});
+
+}
